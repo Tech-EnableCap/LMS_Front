7a5bc0c0282feef2ed5328a82c57627794ed2ca3
Anish Majumdar <tech@enablecap.in>
2021-05-26 19:27:02 +0530

    Initial UI

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import Sidebar from './sidebar';
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+  const [selectFile, setFile] = useState();
+
+  const handleDis = (e) => {
+    let ip = document.getElementById("fileUpload");
+    ip.click();
+  }
+  
+  const disOnChage = (e) => {
+    let file = e.target.files[0];
+    if(file === undefined)
+      return;
+    let fl = file.name.split(".");
+    let ext = fl[fl.length - 1]
+    if(ext !== "xlsx" && ext !== "xls") {
+      alert("Invalid file format " + ext);
+      return;
+    }
+    console.log(file);
+    let frmData = new FormData();
+    frmData.append(
+      "disbursement", file, file.name
+    );
+    axios.post("http://localhost:5000", frmData).then(() => {
+      alert("Successfully Uploaded.");
+    }).catch(e => {
+      console.log(e);
+      alert("Error occurred.")
+    })
+  }
+  
   return (
     <div className="App">
+      <Sidebar
+       handleDis={handleDis}
+       upDisOnChange={disOnChage}
+       />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>Hi</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ src/sidebar.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg'
+
+function SideBar(props) {
+    return (
+        <div className="sidebar">
+            <div className="sdHeader">
+                ENA LMS
+            </div>
+            <img src={logo} className="prfImg" alt="prf"/>
+            <ul>
+                <li onClick={props.handleDis}>
+                <i className="fa fa-upload"></i>
+                 Upload Disbursement File
+                 <input
+                  hidden
+                  id="fileUpload" 
+                  type="file"
+                  onChange={props.upDisOnChange}
+                  />
+                 </li>
+                <li><i className="fa fa-upload"></i> Upload Candidate Equifax</li>
+                <li><i className="fa fa-line-chart"></i> Analysis</li>
+                <li><i className="fa fa-money"></i> Disbursal MIS</li>
+                <li><i className="fa fa-bank"></i> Bank Upload File</li>
+                <li><i className="fa fa-clock-o"></i> Master Repayment Schedule</li>
+                <li><i className="fa fa-bar-chart"></i> Entitled Score Analysis</li>
+            </ul>
+        </div>
+    );
+}
+
+export default SideBar;
