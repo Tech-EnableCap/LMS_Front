77dffb552cf7d4c69cb548e6f1ab7928d6441f01
Anish Majumdar <tech@enablecap.in>
2021-05-27 18:07:16 +0530

    SearchBar added

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -65,18 +65,70 @@ function Dashboard() {
     }
     //==================================
 
+    //Search raw uploaded record........
+    const search = () => {
+        alert("Searching...");
+    }
+    //==================================
+    let style={
+        borderRight:"2px solid red",
+        borderLeft:"2px solid red",
+        borderTop:"2px solid red",
+        borderRadius:"5px",
+        display: "inline-block",
+        margin: "3px"
+    };
+
+    let srStl = {
+        backgroundColor:"rgba(87,87, 85, 0.2)",
+        paddingLeft:"20px",
+        paddingRight:"20px",
+        paddingTop:"3px",
+        paddingBottom:"3px",
+    }
+
+    let btnStl = {
+        width:"75px",
+        height:"40px",
+        borderRadius:"20px",
+        display: "inline-block"
+    }
     return (
         <div className="dashboard">
-            <RepUp 
-            icon="fa fa-upload"
-            wdLabl="Upload Disbursement File"
-            onChange={disOnChange}
-            />
-            <RepUp 
-            icon="fa fa-upload"
-            wdLabl="Upload Candidate Equifax"
-            onChange={eqOnChange}
-            />
+            <div className="dashHeader">
+                <RepUp 
+                icon="fa fa-upload"
+                wdLabl="Upload Disbursement File"
+                onChange={disOnChange}
+                />
+                <RepUp 
+                icon="fa fa-upload"
+                wdLabl="Upload Candidate Equifax"
+                onChange={eqOnChange}
+                />
+            </div>
+            <div className="dashBody">
+                <div className="searchBar" style={srStl}>
+                    <h2>Search using...</h2>
+                    <ul>
+                        <div style={style}>
+                            <li><label>Loan ID</label><br/> <input type="text" id="srLID" /></li>
+                        </div>
+                        <div style={style}>
+                        <li><label>First Name</label><br/> <input type="text" id="fname" /></li>
+                        <li><label>Last Name</label><br/> <input type="text" id="lname" /></li>
+                        </div>
+                        <div style={style}>
+                        <li><label>Start Date</label><br/> <input type="date" id="stDate" /></li>
+                        <li><label>End Date</label><br/> <input type="date" id="enDate" /></li>
+                        </div><br/>
+                        <li style={{
+                            display:"block",
+                            textAlign: "right"
+                        }}><button style={btnStl} onClick={search}>Search</button></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
         
